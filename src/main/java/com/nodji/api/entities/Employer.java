898b49d29dc802ce13f80ce9dbaf21362dc3e52f
nodji <nodji.junior@senior.com.br>
2019-08-24 17:26:50 -0300

    Readme

--- src/main/java/com/nodji/api/entities/Employer.java
+++ src/main/java/com/nodji/api/entities/Employer.java
@@ -6,10 +6,19 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import com.nodji.api.enums.ProfileEnum;
@@ -24,7 +33,7 @@ public class Employer implements Serializable{
 	private UUID id;
 	private String name;
 	private String email;
-	private String senha;
+	private String pass;
 	private String code;
 	private BigDecimal hourValue;
 	private Float hourWorked;
@@ -46,7 +55,8 @@ public class Employer implements Serializable{
 	public void setId(UUID id) {
 		this.id = id;
 	}
-	
+
+	@Column(name="name", nullable=false)
 	public String getName() {
 		return name;
 	}
@@ -55,6 +65,7 @@ public class Employer implements Serializable{
 		this.name = name;
 	}
 
+	@Column(name="email", nullable=false)
 	public String getEmail() {
 		return email;
 	}
@@ -62,15 +73,18 @@ public class Employer implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getSenha() {
-		return senha;
+	
+	@Column(name="pass", nullable=false)
+	public String getPass() {
+		return pass;
 	}
 
-	public void setSenha(String senha) {
-		this.senha = senha;
+	public void setPass(String pass) {
+		this.pass = pass;
 	}
 
+
+	@Column(name="code", nullable=false)
 	public String getCode() {
 		return code;
 	}
@@ -79,6 +93,7 @@ public class Employer implements Serializable{
 		this.code = code;
 	}
 
+	@Column(name="hourValue", nullable=true)
 	public BigDecimal getHourValue() {
 		return hourValue;
 	}
@@ -87,6 +102,7 @@ public class Employer implements Serializable{
 		this.hourValue = hourValue;
 	}
 
+	@Column(name="hourWorked", nullable=true)
 	public Float getHourWorked() {
 		return hourWorked;
 	}
@@ -95,6 +111,7 @@ public class Employer implements Serializable{
 		this.hourWorked = hourWorked;
 	}
 
+	@Column(name="hourLunch", nullable=true)
 	public Float getHourLunch() {
 		return hourLunch;
 	}
@@ -103,6 +120,8 @@ public class Employer implements Serializable{
 		this.hourLunch = hourLunch;
 	}
 
+	@Enumerated(EnumType.STRING)
+	@Column(name="profile", nullable=false)
 	public ProfileEnum getProfile() {
 		return profile;
 	}
@@ -111,6 +130,7 @@ public class Employer implements Serializable{
 		this.profile = profile;
 	}
 
+	@Column(name="dateCreated", nullable=false)
 	public Date getDateCreated() {
 		return dateCreated;
 	}
@@ -119,6 +139,7 @@ public class Employer implements Serializable{
 		this.dateCreated = dateCreated;
 	}
 
+	@Column(name="dateUpdated", nullable=false)
 	public Date getDateUpdated() {
 		return dateUpdated;
 	}
@@ -127,6 +148,7 @@ public class Employer implements Serializable{
 		this.dateUpdated = dateUpdated;
 	}
 
+	@ManyToOne(fetch=FetchType.EAGER)
 	public Company getCompany() {
 		return company;
 	}
@@ -135,6 +157,7 @@ public class Employer implements Serializable{
 		this.company = company;
 	}
 
+	@OneToMany(mappedBy="employer", fetch=FetchType.LAZY, cascade = CascadeType.ALL)
 	public List<Register> getRegisters() {
 		return registers;
 	}
@@ -143,4 +166,23 @@ public class Employer implements Serializable{
 		this.registers = registers;
 	}
 
+	@PreUpdate
+	public void preUpdate() {
+		dateUpdated = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date actual = new Date();
+		dateCreated = actual;
+		dateUpdated = actual;
+	}
+
+	@Override
+	public String toString() {
+		return "Employer [id=" + id + ", name=" + name + ", email=" + email + ", pass=" + pass + ", code=" + code
+				+ ", hourValue=" + hourValue + ", hourWorked=" + hourWorked + ", hourLunch=" + hourLunch + ", profile="
+				+ profile + ", dateCreated=" + dateCreated + ", dateUpdated=" + dateUpdated + "]";
+	}	
+	
 }
--- src/main/java/com/nodji/api/entities/Register.java
+++ src/main/java/com/nodji/api/entities/Register.java
@@ -1,7 +1,138 @@
 package com.nodji.api.entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.nodji.api.enums.TypeEnum;
+
+@Entity
+@Table(name = "register")
 public class Register implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1028996421343658863L;
+
+	private UUID id;
+	private Date dateRegister;
+	private String description;
+	private String local;
+	private TypeEnum type;
+	private Date dateCreated;
+	private Date dateUpdated;
+	private Employer employer;
+	
+	public Register() {
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="dateRegister", nullable=false)
+	public Date getDateRegister() {
+		return dateRegister;
+	}
+
+	public void setDateRegister(Date dateRegister) {
+		this.dateRegister = dateRegister;
+	}
+
+	@Column(name="description", nullable=false)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column(name="local", nullable=false)
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@Column(name="type", nullable=false)
+	public TypeEnum getType() {
+		return type;
+	}
+
+	public void setType(TypeEnum type) {
+		this.type = type;
+	}
+
+	@Column(name="dateCreated", nullable=false)
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	@Column(name="dateUpdated", nullable=false)
+	public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+
+	@ManyToOne(fetch=FetchType.EAGER)
+	public Employer getEmployer() {
+		return employer;
+	}
+
+	public void setEmployer(Employer employer) {
+		this.employer = employer;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		dateUpdated = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date actual = new Date();
+		dateCreated = actual;
+		dateUpdated = actual;
+	}
+
+	@Override
+	public String toString() {
+		return "Register [id=" + id + ", dateRegister=" + dateRegister + ", description=" + description + ", local="
+				+ local + ", type=" + type + ", dateCreated=" + dateCreated + ", dateUpdated=" + dateUpdated + "]";
+	}
+	
 }
